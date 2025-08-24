16bb53226a397c8bb2baa5b79869186408e25795
swaroop <swaroopburre@gmail.com>
2025-08-24 19:55:31 +0530

    Update CI workflow and add CMakeLists.txt, update test runner

--- student-test/assignment1/Test_validate_username_Runner.c
+++ student-test/assignment1/Test_validate_username_Runner.c
@@ -78,7 +78,7 @@ static void run_test(UnityTestFunction func, const char* name, int line_num)
  int Test_validate_username_main(void);
 int Test_validate_username_main(void)
 {
-  UnityBegin("/home/swaroop/assignment-autotest/../student-test/assignment1/Test_validate_username.c");
+  UnityBegin("/home/swaroop/projects/assignment-1/assignment-autotest/../student-test/assignment1/Test_validate_username.c");
   run_test(test_validate_my_username, "test_validate_my_username", 15);
 
   return UnityEnd();
